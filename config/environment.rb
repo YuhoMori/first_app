9c075ece82260d944c8b406fbc8052f3c6d60d37
YuhoMori <morimo07@outlook.jp>
2015-08-01 20:28:22 +0900

    Resolve conflict

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-<<<<<<< HEAD
 FirstApp::Application.initialize!
-=======
-FirstApp::Application.application.initialize!
->>>>>>> ab72c1d54f9772066d471122b68358929e590b7b
